bc734e4a6e40d04d22e6c603b7dacf4d59e12f3b
Aka005 <epj5179@psu.edu>
2025-04-20 21:54:52 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,10 @@
+function openTab(tabId) {
+    const tabs = document.querySelectorAll('.tab-content');
+    const buttons = document.querySelectorAll('.tab-button');
+    tabs.forEach(tab => tab.classList.remove('active'));
+    buttons.forEach(btn => btn.classList.remove('active'));
+
+    document.getElementById(tabId).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
 
